510c5208f268a9ec07f1d5f1b8aac0362782913d
unknown <arthuromgnh@gmail.com>
2022-05-19 14:00:52 +0300

    Added new settings file

--- taxi_service/old_settings/prod.py
+++ taxi_service/old_settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 import dj_database_url
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 DEBUG = False
 ALLOWED_HOSTS = ["taxi-service-proj.herokuapp.com", '127.0.0.1', "https://taxi-service-proj.herokuapp.com/"]
 
@@ -12,5 +12,5 @@ DATABASES = {
     "default": None
 }
 
-DATABASES["default"] = dj_database_url.config(default="postgres://psclqdvh:7HJU3cp8P00cimDn4HigTSpfN9nC0MMW@abul.db.elephantsql.com/psclqdvh", conn_max_age=600)
+
 
