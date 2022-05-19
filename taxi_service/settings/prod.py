1c594ae71355d2b56f9164d5f29a3c7276a42012
unknown <arthuromgnh@gmail.com>
2022-05-19 10:49:21 +0300

    Changed allowed hosts for prod build

--- taxi_service/settings/prod.py
+++ taxi_service/settings/prod.py
@@ -3,7 +3,7 @@ import dj_database_url
 
 
 DEBUG = False
-ALLOWED_HOSTS = ["taxi-service-proj.herokuapp.com", '127.0.0.1']
+ALLOWED_HOSTS = ["taxi-service-proj.herokuapp.com", '127.0.0.1', "https://taxi-service-proj.herokuapp.com/"]
 
 DATABASES = {
     "default": None
