d679fb4289f5709a4c2021de50e72f798af55e91
unknown <arthuromgnh@gmail.com>
2022-05-19 10:27:05 +0300

    Added ordering to car and driver

--- taxi_service/settings/prod.py
+++ taxi_service/settings/prod.py
@@ -6,15 +6,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["taxi-service-proj.herokuapp.com", '127.0.0.1']
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "ProductionDB",
-        "USER": "psclqdvh ",
-        "PASSWORD": "7HJU3cp8P00cimDn4HigTSpfN9nC0MMW",
-        "HOST": "abul.db.elephantsql.com/psclqdvh",
-        "PORT": 5432,
-
-    }
+    "default": None
 }
 
 DATABASES["default"] = dj_database_url.config(default="postgres://psclqdvh:7HJU3cp8P00cimDn4HigTSpfN9nC0MMW@abul.db.elephantsql.com/psclqdvh", conn_max_age=600)
